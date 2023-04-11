7530f93684bd3ae24fbd6a55441ed775490deac5
zcrowleyTheMark <zcrowley@themarkusa.com>
2023-04-11 13:36:29 -0500

    Fixed notes in stacked bar chart and started testing

--- R/stackedBarChart.R
+++ R/stackedBarChart.R
@@ -2,16 +2,21 @@
 #'
 #' @param df A tibble/dataframe of survey items that are categorical/character variables, in 5 point scales and pre-post, that will be inserted into a stacked bar chart with The Mark USA branding.
 #'
-#'@param set_5_levels character vector of 5 levels to set the scale for the plot
+#' @param set_5_levels character vector of 5 levels to set the scale for the plot
 #'
 #' @return A ggplot object that plots the items into a stacked bar chart and can be exported.
 #' @export
 #'
 #' @examples
-#' items <- dplyr::tibble(cat_Pre_Sources = c("Good","Moderate","Minimal","Slight","Slight","Moderate","Good"),
-#'               cat_Post_Sources = c("Good","Good","Minimal","Moderate","Moderate","Good","Extensive"),
-#'               cat_Pre_Orgs = c("Good","Moderate","Minimal","Slight","Slight","Moderate","Moderate"),
-#'               cat_Post_Orgs = c("Good","Moderate","Minimal","Moderate","Moderate","Good","Extensive"))
+#' items <- dplyr::tibble(
+#' cat_Pre_Sources =
+#' c("Good","Moderate","Minimal","Slight","Slight","Moderate","Good"),
+#' cat_Post_Sources =
+#' c("Good","Good","Minimal","Moderate","Moderate","Good","Extensive"),
+#' cat_Pre_Orgs =
+#' c("Good","Moderate","Minimal","Slight","Slight","Moderate","Moderate"),
+#' cat_Post_Orgs =
+#' c("Good","Moderate","Minimal","Moderate","Moderate","Good","Extensive"))
 #' levels_min_ext = c('Minimal', 'Slight', 'Moderate', 'Good', 'Extensive')
 #' stackedBarChart(items,levels_min_ext)
 stackedBarChart <- function(df, set_5_levels) {
@@ -26,32 +31,32 @@ stackedBarChart <- function(df, set_5_levels) {
 
     new_df <-  {{df}} %>%
         tidyr::pivot_longer(tidyselect::everything(), names_to = "question", values_to = "response") %>%
-        dplyr::mutate(question = stringr::str_remove(question, "cat_")) %>%
-        tidyr::separate(question, into = c("timing", "question"), sep = "_") %>%
-        dplyr::group_by(question, timing, response) %>%
+        dplyr::mutate(question = stringr::str_remove(.data$question, "cat_")) %>%
+        tidyr::separate(.data$question, into = c("timing", "question"), sep = "_") %>%
+        dplyr::group_by(.data$question, .data$timing, .data$response) %>%
         dplyr::count(name = "n_answers") %>%
         dplyr::ungroup() %>%
-        dplyr::group_by(question, timing) %>%
-        dplyr::mutate(percent_answers = n_answers / sum(n_answers)) %>%
+        dplyr::group_by(.data$question, .data$timing) %>%
+        dplyr::mutate(percent_answers = .data$n_answers / sum(.data$n_answers)) %>%
         dplyr::ungroup() %>%
-        dplyr::mutate(percent_answers_label = scales::percent(percent_answers, accuracy = 1),
-               timing = factor(timing, levels = c("Pre","Post")),
-               response = factor(response, levels = set_5_levels),
-               label_color = dplyr::if_else(response == levels(response)[2], "black", "white"))
+        dplyr::mutate(percent_answers_label = scales::percent(.data$percent_answers, accuracy = 1),
+                      timing = factor(.data$timing, levels = c("Pre","Post")),
+                      response = factor(.data$response, levels = set_5_levels),
+                      label_color = dplyr::if_else(.data$response == levels(.data$response)[2], "black", "white"))
 
-    N_df <- new_df %>% dplyr::select(-c(percent_answers,percent_answers_label)) %>%
-        tidyr::pivot_wider(names_from = c(question,timing), values_from = c(n_answers)) %>%
+    N_df <- new_df %>% dplyr::select(-c(.data$percent_answers,.data$percent_answers_label)) %>%
+        tidyr::pivot_wider(names_from = c(.data$question,.data$timing), values_from = c(.data$n_answers)) %>%
         dplyr::summarise(dplyr::across(tidyselect::where(is.numeric), ~ sum(.x, na.rm = TRUE))) %>% max()
 
     stacked_bar_chart <- new_df %>%
-        ggplot2::ggplot(ggplot2::aes(x = question, y = percent_answers, fill = response, label = n_answers)) +
+        ggplot2::ggplot(ggplot2::aes(x = .data$question, y = .data$percent_answers, fill = .data$response, label = .data$n_answers)) +
         ggplot2::geom_col(width = 3, position = ggplot2::position_stack(reverse = TRUE)) +
-        ggplot2::geom_text(ggplot2::aes(y = percent_answers, label = n_answers, group = response, color = label_color),
+        ggplot2::geom_text(ggplot2::aes(y = .data$percent_answers, label = .data$n_answers, group = .data$response, color = .data$label_color),
                            family = "Gill Sans MT", fontface = "bold",
                   position = ggplot2::position_stack(vjust = .5,reverse = T), size = 3) +
         ggplot2::scale_color_manual(values = c('black','white')) +
         ggplot2::coord_flip() +
-        ggh4x::facet_nested_wrap(dplyr::vars(question,timing), ncol = 1, scales = "free", strip.position = "left",
+        ggh4x::facet_nested_wrap(dplyr::vars(.data$question,.data$timing), ncol = 1, scales = "free", strip.position = "left",
                           strip = ggh4x::strip_nested(text_y = texts, by_layer_y = TRUE)) +
         ggplot2::scale_fill_manual(values = fiveScale_theMark_colors, drop = FALSE, labels = function(response) stringr::str_wrap(response, width = 10)) +
         ggplot2::guides(color = "none",fill = ggh4x::guide_stringlegend(size = 12, family = "Gill Sans MT", face = "bold", hjust = 0, vjust = 0, ncol = 5,
