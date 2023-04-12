0c113b057fddbf02645960c112f3086463072282
zcrowleyTheMark <zcrowley@themarkusa.com>
2023-04-12 12:13:51 -0500

    updated R package name to TheMarkUSA

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,7 +7,7 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(themark)
+library(TheMarkUSA)
 
-test_check("themark")
+test_check("TheMarkUSA")
 
--- tests/testthat/test-stackedBarChart.R
+++ tests/testthat/test-stackedBarChart.R
@@ -1,7 +1,7 @@
 test_that("plots have known output", {
     disp_hist_base <- function() hist(mtcars$disp)
     vdiffr::expect_doppelganger("disp-histogram-base", disp_hist_base)
-    disp_hist_ggplot <- ggplot2::ggplot(mtcars, ggplot2::aes(disp)) + ggplot2::geom_histogram(binwidth=30)
+    disp_hist_ggplot <- ggplot2::ggplot(mtcars, ggplot2::aes(disp)) + ggplot2::geom_histogram(binwidth = 30)
     vdiffr::expect_doppelganger("disp-histogram-ggplot", disp_hist_ggplot)
 })
 
