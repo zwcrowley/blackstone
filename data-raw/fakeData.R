d9f7566e851e9681b0f184a675f777d0777011da
Zack Crowley <zcrowley@blackstoneevaluation.com>
2024-06-26 10:27:04 -0500

    Set sm demo data in `data-raw folder

--- data-raw/fakeData.R
+++ data-raw/fakeData.R
@@ -1,8 +1,6 @@
 # Creating fake data for Rmd Template use:
 # Zack Crowley
- 
-# Set here:
-here::i_am("extdata/fakeData.R")
+
 
 # Load libraries:
 library(here)
@@ -45,14 +43,14 @@ levels_never_always <- c("Never", "Rarely", "Sometimes", "Frequently", "Always")
 levels_useful <- c("Not at all useful", "Slightly useful", "Somewhat useful", "Very useful", "Extremely useful")
 
 # Open-ended text variables- follow-up questions to explain when "Not at all useful" selected from the any of the Training usefulness items:
-# (made up of non-sense text from jane austin books) 
+# (made up of non-sense text from jane austin books)
 # Responsible_oe, Ethics_oe, Standards_oe, Practices_oe, Morals_oe
 words <- austen_books() %>%
     group_by(book) %>%
     mutate(linenumber = row_number(),
            chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]",
                                                    ignore_case = TRUE)))) %>%
-    ungroup() %>% filter(book == "Pride & Prejudice") %>% unnest_tokens(word, text) %>% select(word) %>% deframe() 
+    ungroup() %>% filter(book == "Pride & Prejudice") %>% unnest_tokens(word, text) %>% select(word) %>% deframe()
 # Create a tibble of 1000, that is made up of random text with words ranging in length from 12 to 36:
 oe_data <- purrr::map(.x = 1:1000, .f = function(x){paste(sample(words, size = sample(12:36,1, replace = TRUE), replace = TRUE), sep = ' ', collapse = ' ')}) %>% list_c() %>% as_tibble_col(column_name = "oe_text")
 
@@ -76,9 +74,9 @@ data <- dplyr::tibble(
     Standards = sample(levels_useful, size = 100, replace = TRUE, prob = c(0.1, 0.1, 0.2, 0.3, 0.3)),
     Practices = sample(levels_useful, size = 100, replace = TRUE, prob = c(0.1, 0.1, 0.2, 0.3, 0.3)),
     Morals = sample(levels_useful, size = 100, replace = TRUE, prob = c(0.05, 0.05, 0.2, 0.3, 0.4)),
-    Responsible_oe = ifelse(Responsible == "Not at all useful", sample(oe_data$oe_text), NA_character_), 
+    Responsible_oe = ifelse(Responsible == "Not at all useful", sample(oe_data$oe_text), NA_character_),
     Ethics_oe = ifelse(Ethics == "Not at all useful", sample(oe_data$oe_text), NA_character_),
-    Standards_oe = ifelse(Standards == "Not at all useful", sample(oe_data$oe_text), NA_character_), 
+    Standards_oe = ifelse(Standards == "Not at all useful", sample(oe_data$oe_text), NA_character_),
     Practices_oe = ifelse(Practices == "Not at all useful", sample(oe_data$oe_text), NA_character_),
     Morals_oe = ifelse(Morals == "Not at all useful", sample(oe_data$oe_text), NA_character_)
 )
---
+++ data-raw/sm_data_demo.R
@@ -0,0 +1,63 @@
+# Fake SurveyMonkey and demograpich data for `bre` package:
+# Zack Crowley
+# Creating a fake dataset to use in the 'bre' package, this will be all the standard SurveyMonkey data fields
+# and three demographic variables: This will be the basis for pre-post data generated in the two other scripts in
+# `data-raw`
+
+# Prevents sci notation and sets the output of decimals to 4 (0.0000):
+options(scipen = 999, digits = 4)
+# Set seed to reproduce data:
+set.seed(04240629)
+# Load Libraries:
+library(readr)
+library(dplyr)
+library(charlatan)
+
+# setup the providers for charlatan
+ip <- InternetProvider_en_US$new()
+lp <- LoremProvider_en_US$new()
+np <- NumericsProvider$new()
+person <- PersonProvider_en_US$new()
+
+# Total N of 100 clients:
+n <- 100
+
+
+# SurveyMonkey respondent_id, numeric vector
+respondent_id <- as.numeric(114628000001:114628000100)
+
+# IP address and emails:
+email_address <- sapply(1:n, function(x) ip$email())
+ip_address  <- sapply(1:n, function(x) ip$ipv4())
+
+# Generate Unique ID, generate 100 with starting number of 24290001:
+unique_id <- as.numeric(24290001:24290100)
+
+# Names from the `charlatan` package, create 300 male and 300 female names and last names as vectors:
+female_names <- sapply(1:n , function(x) person$first_name_female())
+last_names_female <- sapply(1:n , function(x) person$last_name_female())
+male_names <- sapply(1:n , function(x) person$first_name_male())
+last_names_male <- sapply(1:n , function(x) person$last_name_male())
+
+# Add respondent_id, email_address,ip_address, unique_id  to tibble and generate gender, first and last names, ethnicity, and first_gen using sample of size 100:
+sm_data_demo <- tibble(
+    respondent_id = respondent_id, # from above
+    collector_id = as.numeric(431822954), # same number- assuming that is how SM does it
+    start_date = sample(seq(as.Date("2024-06-01"), as.Date("2024-06-25"), by = "day"), size = n, replace = TRUE),
+    end_date = start_date + 1,
+    ip_address = ip_address, # from above
+    email_address = email_address, # from above
+    gender = sample(c("Female","Male","Non-binary", "Do not wish to specify"), size = n, replace = TRUE, prob = c(0.52, 0.44, 0.02, 0.02)),
+    first_name = if_else(gender == "Female", sample(female_names), sample(male_names)), # sample from female names vector if gender is female, otherwise use male name vector.
+    last_name = if_else(gender == "Female", sample(last_names_female), sample(last_names_male)), # sample from female last names vector if gender is female, otherwise use  male last name vector.
+    unique_id = unique_id, # from above
+    ethnicity = sample(c("White (Non-Hispanic/Latino)", "Asian", "Black",  "Hispanic or Latino", "American Indian or Alaskan Native",
+                         "Native Hawaiian or other Pacific Islander", "Do not wish to specify"), size = n, replace = TRUE, prob = c(0.30, 0.20, 0.15, 0.20, 0.05, 0.06, 0.03)),
+    first_gen = sample(c("Yes", "No", "I'm not sure"), size = n, replace = TRUE, prob = c(0.52, 0.44, 0.03))
+) %>% select(respondent_id, collector_id, start_date, end_date, ip_address, email_address, first_name, last_name, unique_id, gender, ethnicity, first_gen) # set correct order
+sm_data_demo
+
+# Write out data tibble with sm data and demos to `data-raw` folder:
+readr::write_csv(sm_data_demo, "sm_data_demo.csv")
+# For this data set, it will NOT be used in the package, but code below would do that:
+# usethis::use_data(sm_data_demo, overwrite = TRUE)
